dd492d5378779b54ef11e82772af95b77f4c0cf7
Gabriel-AndradeWeb <gabrielloliveira741@gmail.com>
2022-05-19 19:55:58 -0300

    Atualização

---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Tênis Nike Sportswear Air",
+      price: 300,
+      image: "./images/produtos/tenis-air-vapormax-2021-fk-DH4084-100-1-11628872325.jpg",
+      id_categorys: 1,
+      name_categorys: "Brasileiros",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Tênis Nike Air Max 97",
+      price: 350,
+      image: "./images/produtos/tenis-nike-air-max-97.jpg",
+      id_categorys: 1,
+      name_categorys: "Brasileiros",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "nacional",
+      fk_idcategorys: 1,
+      name_product: "Tênis Nike Sportswear Air Max",
+      price: 230,
+      image: "./images/produtos/tenis-nike-sportswear-air-max-2021-masculino-DH4245-001-1-11633451368.jpg",
+      id_categorys: 1,
+      name_categorys: "Brasileiros",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Tênis Nike Air Vapormax Evo",
+      price: 399.99,
+      image: "./images/produtos/tenis-nike-air-vapormax-evo-masculino-CZ1924-001-1.jpg",
+      id_categorys: 2,
+      name_categorys: "Europeus",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Tênis Nike Dunk High 1985 SP",
+      price: 350.99,
+      image: "./images/produtos/tenis-nike-dunk-hi-1985-sp-DD9404-700-1-11628003814.jpg",
+      id_categorys: 2,
+      name_categorys: "Europeus",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "europeu",
+      fk_idcategorys: 2,
+      name_product: "Tênis Nike Manoa",
+      price: 299.99,
+      image: "./images/produtos/tenis-nike-manoa-masculino-454350-700-1.jpg",
+      id_categorys: 2,
+      name_categorys: "Europeus",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "historica",
+      fk_idcategorys: 3,
+      name_product: "Tênis Nike Air Presto",
+      price: 200,
+      image: "./images/produtos/air-presto-CJ1229-700-1.jpg",
+      id_categorys: 3,
+      name_categorys: "Camisas históricas",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "historica",
+      fk_idcategorys: 3,
+      name_product: "Tênis Nike Sportswear Air Max",
+      price: 450,
+      image: "./images/produtos/tenis-air-max-95-og-DC9412-001-1-11628869763.jpg",
+      id_categorys: 3,
+      name_categorys: "Camisas históricas",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "selecao",
+      fk_idcategorys: 4,
+      name_product: "Tênis Nike Air Max 97",
+      price: 350,
+      image: "./images/produtos/tenis-nike-air-max-97-DH0006-100-1-11623332588.jpg",
+      id_categorys: 4,
+      name_categorys: "Seleções nacionais",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "selecao",
+      fk_idcategorys: 4,
+      name_product: "Tênis Nike Air Max Infinity 2",
+      price: 399,
+      image: "./images/produtos/tenis-nike-air-max-infinity-2-CU9452-002-1.jpg",
+      id_categorys: 4,
+      name_categorys: "Seleções nacionais",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "casual",
+      fk_idcategorys: 5,
+      name_product: "Tênis Nike Air Force 1 '07",
+      price: 180,
+      image: "./images/produtos/tenis-nike-air-force-1-07-masculino.jpg",
+      id_categorys: 5,
+      name_categorys: "Casuais",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "casual",
+      fk_idcategorys: 5,
+      name_product: "Tênis Nike Court Vision Low",
+      price: 189.99,
+      image: "./images/produtos/tenis-nike-court-vision-lo-DM8681-001-1-11628873177.jpg",
+      id_categorys: 5,
+      name_categorys: "Casuais",
+      selected: false
+    },
+];
+
+export default productsList;
